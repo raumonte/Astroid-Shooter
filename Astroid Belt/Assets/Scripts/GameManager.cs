b3cb885b8298ec8851c1f9174a15b08df01e8b47
Raul Montes <montesraul7@gmail.com>
2020-06-03 16:00:05 -0700

    Added in Monday's code and Wednesday's Code

---
+++ Astroid Belt/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public int score;
+    public GameObject player;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }   
+        else
+        {
+            Destroy(this.gameObject);
+            Debug.LogError("[GameManager] Attempted to create a second game manager.");
+        }
+    }
+}
---
+++ Astroid Belt/Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float turnSpeed = 90f;
+    public float moveSpeed = 5f;
+    private Transform tf;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager.instance.player = this.gameObject;
+        tf = gameObject.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            tf.Rotate(0, 0, turnSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            tf.Rotate(0, 0, turnSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            tf.Rotate(0, 0, turnSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            tf.Rotate(0, 0, turnSpeed * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+        void Shoot()
+        {
+            Debug.Log("Bang Bang");
+        }
+    }
+}
